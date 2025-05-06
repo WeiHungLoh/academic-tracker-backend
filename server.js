a63574feb71bcec023b1f7a250f1bc222b2fdb0a
WeiHungLoh <weihung789@hotmail.com>
2025-05-06 23:56:00 +0800

    Remove origin from cors

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const verifyToken = require("./middleware/verifyToken");
 
 connectDB();
 const app = express();
-app.use(cors({
-    origin: ["http://localhost:3000", "https://academic-tracker-backend.onrender.com"],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", require("./routes/auth"));
